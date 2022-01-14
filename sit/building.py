e17144de6872c8f9c997eb275371cfe0ad34bf6f
John Barton <john@mediayoucanfeel.com>
2022-01-13 19:56:11 -0500

    Add initial SitObject and Building classes

---
+++ sit/building.py
@@ -0,0 +1,23 @@
+from sit_object import SitObject
+
+
+class Building(SitObject):
+
+    def __init__(self, db_id: int = None, number: str = None):
+        super().__init__(db_id)
+        self.__number = number
+
+    @property
+    def number(self):
+        if self.__number is None:
+            self.populate()
+
+        return self.__number
+
+    def populate(self):
+        pass
+
+    def _get_create_params_dict(self):
+        return {
+            'number': self.number
+        }
---
+++ sit/sit_object.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+import sqlite3 as sl
+
+
+class SitObject(ABC):
+
+    db_name = 'suntree-inventory-tracker.db'
+    table_name = None
+
+    def __init__(self, db_id: int = None):
+        self.id = db_id
+
+    def create(self):
+        create_params = self._get_create_params_dict()
+
+        col_names = [key for key in create_params]
+        col_names_for_query = ', '.join(col_names)
+
+        q_marks = ['?' for key in create_params]
+        q_marks_for_query = ', '.join(q_marks)
+
+        query = f'INSERT INTO {self.__class__.table_name} ({col_names_for_query}) VALUES ({q_marks_for_query});'
+        return self.__class__.run_query(query, tuple(create_params.values()))
+
+    @abstractmethod
+    def populate(self):
+        pass
+
+    @abstractmethod
+    def _get_create_params_dict(self):
+        pass
+
+    @staticmethod
+    def get_by_id(db_id: int):
+        pass
+
+    @staticmethod
+    def get_db_conn():
+        return sl.connect(SitObject.db_name)
+
+    @classmethod
+    def run_query(cls, query: str, params: tuple = None):
+        conn = cls.get_db_conn()
+
+        with conn:
+            conn.row_factory = sl.Row
+            cursor = conn.cursor()
+
+            if params:
+                # If given params, pass them to the execute function
+                cursor.execute(query, params)
+            else:
+                # Otherwise, just execute the query without parameters
+                cursor.execute(query)
+
+            # Get & return the results
+            result = cursor.fetchall()
+            return result
