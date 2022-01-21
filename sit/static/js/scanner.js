b36891799306d23f243b6c995dec91906d43c373
John Barton <john@mediayoucanfeel.com>
2022-01-21 14:43:42 -0500

    Add support for changing cameras during scanning

--- sit/static/js/scanner.js
+++ sit/static/js/scanner.js
@@ -15,16 +15,57 @@ function hasGetUserMedia() {
 }
 
 function initCapture() {
-    console.log("Starting capture...");
-    const constraints = {
-        video:  { height: 360, width: 480 }
-    };
-
+    // Get device info
+    const videoSelect = document.querySelector("select#videoSource")
     const video = document.querySelector("video");
 
-    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    navigator.mediaDevices
+        .enumerateDevices()
+        .then(gotDevices)
+        .then(getStream);
+
+    videoSelect.onchange = getStream;
+
+    function gotDevices(deviceInfos) {
+        for (let i = 0; i !== deviceInfos.length; ++i) {
+            const deviceInfo = deviceInfos[i];
+            const option = document.createElement("option");
+            option.value = deviceInfo.deviceId;
+            if (deviceInfo.kind === "videoinput") {
+                option.text = deviceInfo.label || "camera " + (videoSelect.length + 1);
+                videoSelect.appendChild(option);
+            } else {
+                console.log("Found another kind of device: ", deviceInfo);
+            }
+        }
+    }
+
+    function getStream() {
+        if (window.stream) {
+            window.stream.getTracks().forEach(function (track) {
+                track.stop();
+            });
+        }
+
+        const constraints = {
+            video: {
+                deviceId: { exact: videoSelect.value },
+                height: 360,
+                width: 480
+            },
+        };
+
+        navigator.mediaDevices
+            .getUserMedia(constraints)
+            .then(gotStream);
+    }
+
+    function gotStream(stream) {
+        window.stream = stream; // make stream available to console
         video.srcObject = stream;
-    });
+    }
+
+    console.log("Starting capture...");
 
     // Begin capturing every second
     const interval = setInterval(function() {
@@ -55,15 +96,6 @@ function sendScreenshot() {
     });
 }
 
-function handleScreenshots() {
-    const screenshotButton = document.querySelector("#screenshot-btn");
-    const video = document.querySelector("video");
-    const img = document.querySelector("#screenshot-img");
-
-    screenshotButton.onclick = video.onclick = function () {
-        img.src = getScreenshot();
-    };
-}
 
 function getScreenshot() {
     const video = document.querySelector("video");
