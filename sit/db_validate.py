4db1993a50759769d2285b197de41e0bf23e85c1
John Barton <john@mediayoucanfeel.com>
2022-02-24 19:04:28 -0500

    Add SQL support for labels

--- sit/db_validate.py
+++ sit/db_validate.py
@@ -18,40 +18,59 @@ def validate(db_name):
         logging.warning('Could not find database - will initialize an empty one!')
         conn = sl.connect(db_name)
 
-        # BUILDING
-        with conn:
-            conn.execute("""
-                CREATE TABLE building (
-                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                    number TEXT
-                );                
-            """)
+    # BUILDING
+    with conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS building (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                number TEXT
+            );                
+        """)
 
-        # ROOM
-        with conn:
-            conn.execute("""
-                CREATE TABLE room (
-                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                    number TEXT,
-                    building INT REFERENCES building(id) ON DELETE CASCADE
-                );                
-            """)
+    # ROOM
+    with conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS room (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                number TEXT,
+                building INT REFERENCES building(id) ON DELETE CASCADE
+            );                
+        """)
 
-        # ITEM
-        with conn:
-            conn.execute("""
-                CREATE TABLE item (
-                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                    description TEXT NOT NULL,
-                    purchase_price TEXT,
-                    purchase_date INTEGER,
-                    room INT REFERENCES room(id) ON DELETE CASCADE,
-                    photo TEXT,
-                    quantity INTEGER
-                );
-            """)
+    # ITEM
+    with conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS item (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                description TEXT NOT NULL,
+                purchase_price TEXT,
+                purchase_date INTEGER,
+                room INT REFERENCES room(id) ON DELETE CASCADE,
+                photo TEXT,
+                quantity INTEGER
+            );
+        """)
 
-        return
+    # LABEL
+    with conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS label (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                text TEXT NOT NULL
+            );
+        """)
+
+    # LABEL-ITEM MAPPING
+    with conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS item_label_map (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                label_id INT NOT NULL,
+                item_id INT NOT NULL,
+                FOREIGN KEY (label_id) REFERENCES label(id),
+                FOREIGN KEY (item_id) REFERENCES item(id)                
+            );
+        """)
 
     check_columns()
 
